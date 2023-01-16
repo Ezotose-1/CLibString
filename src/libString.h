1ee711f8f0a0eec74b25170929e0d1048ce2bd03
Pierre <60015586+Ezotose-1@users.noreply.github.com>
2023-01-16 14:56:47 +0100

    Update libString.h

--- src/libString.h
+++ src/libString.h
@@ -4,6 +4,10 @@
 unsigned int len(char *str);
 unsigned int count(char *str, char *sub);
 int index(char *str, char *sub);
+char *join(char *str, char **iterable, int list_length);
+char *ljust(char *str, size_t width, char fillchar);
+char *lstrip(char *str, char *chars);
+char *removeprefix(char *str, char *prefix);
 
 /* CASE */
 char *capitalize(char *str);
@@ -17,3 +21,6 @@ char *expandtabs(char *str, unsigned int tabsize);
 int endswith(char *str, char *suffix);
 int py_isalnum(char *str);
 int py_isalpha(char *str);
+int py_isascii(char *str);
+int py_isdigit(char *str);
+int isidentifier(char *str);
